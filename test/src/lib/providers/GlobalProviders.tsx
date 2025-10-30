8c4a47969015c1291d628e33c34d151530da237c
siwachs <ssiwach.8888@gmail.com>
2025-10-30 18:42:12 +0530

    Setup Error Boundary

--- test/src/lib/providers/GlobalProviders.tsx
+++ test/src/lib/providers/GlobalProviders.tsx
@@ -1,14 +1,16 @@
 import { type ReactNode } from "react";
 import { ThemeProvider } from "./ThemeProvider";
 
+import { ErrorBoundary } from "@/components/ErrorBoundary";
+
 export function GlobalProviders({
   children,
 }: Readonly<{ children: ReactNode }>) {
   return (
-    // <ErrorBoundary>
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      {children}
-    </ThemeProvider>
-    // </ErrorBoundary>
+    <ErrorBoundary>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        {children}
+      </ThemeProvider>
+    </ErrorBoundary>
   );
 }
